054a206919f6958a35672774c5a3230a40ab9602
Anna-Belle Filion <treblecharger34@hotmail.com>
2025-03-03 15:14:22 -0500

    Created a constant file containing the features and their correlation.

---
+++ notebook/features.py
@@ -0,0 +1,20 @@
+# Defining the correlation features dictionary
+features = {
+    "Tenure Months": 0.352228670113079,
+    "Internet Service_Fiber optic": 0.3080197449448237,
+    "Contract_Two year": 0.30225346934965186,
+    "Payment Method_Electronic check": 0.30191874901128596,
+    "Dependents_Yes": 0.24854222642328383,
+    "Internet Service_No": 0.22789044975609318,
+    "Monthly Charges": 0.19335642223784827,
+    "Paperless Billing_Yes": 0.19182533166647064,
+    "Contract_One year": 0.1778200183499072,
+    "Online Security_Yes": 0.171226291948553,
+    "Tech Support_Yes": 0.16467355590159374,
+    "Senior Citizen_Yes": 0.15088932817647407,
+    "Partner_Yes": 0.15044754495917692,
+    "Payment Method_Credit card (automatic)": 0.13430175066262853,
+    "Payment Method_Mailed check": 0.0916828385884294,
+    "Online Backup_Yes": 0.08225486893814218,
+    "Device Protection_Yes": 0.06615979128029068
+}
